9458100715b00ab6087db5f87e17cf296e66862a
dumavladislav <vladislav.v.duma@gmail.com>
2020-12-30 01:42:46 +0300

    font parameterized

--- DumskyOLED/DumskyOLED.cpp
+++ DumskyOLED/DumskyOLED.cpp
@@ -1,18 +1,26 @@
 #include <DumskyOLED.h>
 
 
-void DumskyOLED::init() {
+void DumskyOLED::init(const uint8_t font_size) {
     // u8g2.setI2CAddress(0x3C);
+    this->font_size = font_size;
+    switch(font_size) {
+        case 10: this->font = u8g2_font_ncenB08_tr; break;
+        case 25: this->font = u8g2_font_crox5tb_tf; break;
+        default: this->font = u8g2_font_ncenB08_tr; break;
+    }
+    
     u8g2.begin();    
     u8g2.clearBuffer();					// clear the internal memory
-    u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
+    u8g2.setFont(this->font);	// choose a suitable font
     //u8g2.setFont(u8g2_font_crox5tb_tf);     // For Motion sensor
 }
 
 
 void DumskyOLED::clear() {
     u8g2.clearBuffer();					// clear the internal memory
-    u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
+    u8g2.setFont(this->font);	// For GPS Tracker
+    // u8g2.setFont(u8g2_font_ncenB08_tr);	// For GPS Tracker
     // u8g2.setFont(u8g2_font_crox5tb_tf);  // For Motion sensor
     currString = 0;
 }
@@ -28,7 +36,8 @@ void DumskyOLED::addString(String str) {
         clear();
         currString = 0;
     } 
-    u8g2.drawStr(0, currString * 10, str.c_str()); 
+    u8g2.drawStr(0, currString * this->font_size, str.c_str()); 
+    // u8g2.drawStr(0, currString * 10, str.c_str()); 
     // u8g2.drawStr(0, currString * 25, str.c_str());  // For Motion sensor
 }
 
--- DumskyOLED/DumskyOLED.h
+++ DumskyOLED/DumskyOLED.h
@@ -5,7 +5,7 @@ class DumskyOLED {
 
 public:
 
-    void init();
+    void init(const uint8_t font_size);
     void clear();
     void addString(String str);
     void renderString(String str);
@@ -15,6 +15,9 @@ private:
 
     uint8_t currString = 0;
     uint8_t width, height = 0;
+
+    uint8_t font_size;
+    const uint8_t *font;
 
     // U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2{U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE};
     U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2{U8G2_R0,  /* reset=*/ U8X8_PIN_NONE};
